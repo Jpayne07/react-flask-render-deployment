5d7aae9595910d072e25f3b6746847f7815a73f6
Jacob <jacobpayne007@gmail.com>
2025-02-18 23:21:46 -0600

    add ignore file and env connections

--- server/app.py
+++ server/app.py
@@ -1,16 +1,22 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy import MetaData
 from models import db
+import os
 # create the app
 app = Flask(__name__)
 metadata = MetaData()
+app.secret_key = os.environ.get('APP_SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 # configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
-# initialize the app with the extension
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    print(f"Starting app on port {port}")
+    app.run(host='0.0.0.0', port=port, debug=True)
